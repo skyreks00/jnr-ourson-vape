f1fc706ea52dccc481ab15e3fead5ce7514f8d5a
skyreks <sunshine.wilmus.mail@gmail.com>
2026-05-12 22:21:20 +0200

    Fix: Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changez en '/nom-du-repo/' si votre repo GitHub n'est pas votre site principal
+  base: '/jnr-ourson-vape/',
 })
